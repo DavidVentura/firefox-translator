d2814bcae76bc222e4a56b93deb50d33c902de3a
David Ventura <david@davidv.dev>
2025-09-28 12:24:31 +0200

    refactor for early return

--- app/src/main/java/dev/davidv/translator/BackgroundTranslationService.kt
+++ app/src/main/java/dev/davidv/translator/BackgroundTranslationService.kt
@@ -36,59 +36,63 @@ class BackgroundTranslationService : Service() {
     flags: Int,
     startId: Int,
   ): Int {
-    if (intent?.action == "dev.davidv.translator.action.TRANSLATE_TEXT") {
-      val textToTranslate = intent.getStringExtra("text_to_translate")
-      val fromLanguageStr = intent.getStringExtra("from_language")
-      val toLanguageStr = intent.getStringExtra("to_language")
-      val fromLanguage = fromLanguageStr?.let { lng -> Language.entries.find { it.code == lng } }
-      val toLanguage = toLanguageStr?.let { lng -> Language.entries.find { it.code == lng } }
-      val receiver = intent.getParcelableExtra<Messenger>("result_receiver")
-      Log.d(TAG, "translate txt: $textToTranslate, $fromLanguage, $toLanguage, cb = ${receiver != null}")
+    if (intent == null) {
+      Log.d(TAG, "Intent is null")
+      stopSelf()
+      return START_NOT_STICKY
+    }
+    if (intent.action != "dev.davidv.translator.action.TRANSLATE_TEXT") {
+      Log.d(TAG, "Unknown action: ${intent?.action}")
+      stopSelf()
+      return START_NOT_STICKY
+    }
+    val textToTranslate = intent.getStringExtra("text_to_translate")
+    val fromLanguageStr = intent.getStringExtra("from_language")
+    val toLanguageStr = intent.getStringExtra("to_language")
+    val fromLanguage = fromLanguageStr?.let { lng -> Language.entries.find { it.code == lng } }
+    val toLanguage = toLanguageStr?.let { lng -> Language.entries.find { it.code == lng } }
+    val receiver = intent.getParcelableExtra<Messenger>("result_receiver")
+    Log.d(TAG, "translate txt: $textToTranslate, $fromLanguage, $toLanguage, cb = ${receiver != null}")
 
-      if (textToTranslate != null && receiver != null) {
-        CoroutineScope(Dispatchers.IO).launch {
-          langStateManager.languageState.first { !it.isChecking }
-          val langs =
-            langStateManager.languageState.value.availableLanguageMap.keys
-              .toList()
-          val from = fromLanguage ?: translationCoordinator.detectLanguageRobust(textToTranslate, null, langs)
-          if (from != null) {
-            val to = toLanguage ?: SettingsManager(applicationContext).settings.value.defaultTargetLanguage
-            val result = translationCoordinator.translateText(from, to, textToTranslate)
-            val translatedText =
-              when (result) {
-                is TranslationResult.Success -> result.result.translated
-                is TranslationResult.Error -> "Error: " + result.message
-                null -> "Error: Translation failed"
-              }
-            val bundleKey =
-              when (result) {
-                is TranslationResult.Success -> "translated_text"
-                else -> "error"
-              }
-            Log.d(TAG, "$bundleKey: $translatedText")
-            val bundle = Bundle()
-            bundle.putString(bundleKey, translatedText)
-            val msg = Message.obtain()
-            msg.data = bundle
-            receiver.send(msg)
-          } else {
-            val bundle = Bundle()
-            bundle.putString("translated_text", "Error: Could not detect language")
-            Log.d(TAG, "Error: Could not detect language")
-            val msg = Message.obtain()
-            msg.data = bundle
-            receiver.send(msg)
-          }
-          stopSelf()
+    if (textToTranslate == null || receiver == null) {
+      stopSelf()
+      return START_NOT_STICKY
+    }
+    CoroutineScope(Dispatchers.IO).launch {
+      langStateManager.languageState.first { !it.isChecking }
+      val langs =
+        langStateManager.languageState.value.availableLanguageMap.keys
+          .toList()
+      val from = fromLanguage ?: translationCoordinator.detectLanguageRobust(textToTranslate, null, langs)
+      if (from == null) {
+        val bundle = Bundle()
+        bundle.putString("translated_text", "Error: Could not detect language")
+        Log.d(TAG, "Error: Could not detect language")
+        val msg = Message.obtain()
+        msg.data = bundle
+        receiver.send(msg)
+        stopSelf()
+        return@launch
+      }
+      val to = toLanguage ?: SettingsManager(applicationContext).settings.value.defaultTargetLanguage
+      val result = translationCoordinator.translateText(from, to, textToTranslate)
+      val translatedText =
+        when (result) {
+          is TranslationResult.Success -> result.result.translated
+          is TranslationResult.Error -> "Error: " + result.message
+          null -> "Error: Translation failed"
         }
-      }
-    } else {
-      if (intent == null) {
-        Log.d(TAG, "Intent is null")
-      } else {
-        Log.d(TAG, "Unknown action: ${intent?.action}")
-      }
+      val bundleKey =
+        when (result) {
+          is TranslationResult.Success -> "translated_text"
+          else -> "error"
+        }
+      Log.d(TAG, "$bundleKey: $translatedText")
+      val bundle = Bundle()
+      bundle.putString(bundleKey, translatedText)
+      val msg = Message.obtain()
+      msg.data = bundle
+      receiver.send(msg)
       stopSelf()
     }
     return START_NOT_STICKY
